9e32e510e272d0966f6290a2a2afa8deefa9180f
xiech <xiechanghong@everimaging.com>
2019-03-27 00:05:55 +0800

    注册中心 & 配置中心

--- jcoffee-config/src/main/java/com/jcoffee/config/JcoffeeConfigApplication.java
+++ jcoffee-config/src/main/java/com/jcoffee/config/JcoffeeConfigApplication.java
@@ -2,8 +2,15 @@ package com.jcoffee.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 配置中心
+ */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class JcoffeeConfigApplication {
 
     public static void main(String[] args) {
