b38ea27e735c5d81c586ab954b71dbfde061a971
xiech <xiechanghong@everimaging.com>
2019-03-27 17:54:00 +0800

    增加业务模块 & 封装公共组件

---
+++ jcoffee-business/jcoffee-business-manage/src/main/java/com/jcoffee/business/manage/JcoffeeBusinessManageApplication.java
@@ -0,0 +1,13 @@
+package com.jcoffee.business.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcoffeeBusinessManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcoffeeBusinessManageApplication.class, args);
+	}
+
+}
