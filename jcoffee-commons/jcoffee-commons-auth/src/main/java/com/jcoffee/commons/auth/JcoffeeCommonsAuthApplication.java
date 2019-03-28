80d6f1270917d51d7956c2b0d4f327ea606794ec
xiech <xiechanghong@everimaging.com>
2019-03-28 12:20:09 +0800

    增加redis基础组件

---
+++ jcoffee-commons/jcoffee-commons-auth/src/main/java/com/jcoffee/commons/auth/JcoffeeCommonsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jcoffee.commons.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcoffeeCommonsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcoffeeCommonsAuthApplication.class, args);
+	}
+
+}
---
+++ jcoffee-commons/jcoffee-commons-redis/src/main/java/com/jcoffee/commons/redis/constant/RedisToolsConstant.java
@@ -0,0 +1,17 @@
+package com.jcoffee.commons.redis.constant;
+
+
+public class RedisToolsConstant {
+    private RedisToolsConstant() {
+        throw new IllegalStateException("Utility class");
+    }
+    /**
+     * single Redis
+     */
+    public final static int SINGLE = 1 ;
+
+    /**
+     * Redis cluster
+     */
+    public final static int CLUSTER = 2 ;
+}
