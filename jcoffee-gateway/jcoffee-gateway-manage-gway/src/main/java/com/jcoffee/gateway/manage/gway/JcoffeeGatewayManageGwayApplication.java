54c90a62eacbea3b595841886cec2510ce6b9d0c
xiech <xiechanghong@everimaging.com>
2019-04-06 18:25:53 +0800

    增加后台网关模块

---
+++ jcoffee-gateway/jcoffee-gateway-manage-gway/src/main/java/com/jcoffee/gateway/manage/gway/JcoffeeGatewayManageGwayApplication.java
@@ -0,0 +1,18 @@
+package com.jcoffee.gateway.manage.gway;
+
+import com.jcoffee.commons.basics.util.port.PortApplicationEnvironmentPreparedEventListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JcoffeeGatewayManageGwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(JcoffeeGatewayManageGwayApplication.class);
+		app.addListeners(new PortApplicationEnvironmentPreparedEventListener());
+		app.run(args);
+	}
+
+}
