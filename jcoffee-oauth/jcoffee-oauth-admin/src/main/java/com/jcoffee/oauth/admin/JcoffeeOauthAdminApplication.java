0ae07f1bc86b7508a02e091632065d56601a3143
xiech <xiechanghong@everimaging.com>
2019-04-09 12:58:40 +0800

    增加oauth admin模块

---
+++ jcoffee-oauth/jcoffee-oauth-admin/src/main/java/com/jcoffee/oauth/admin/JcoffeeOauthAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jcoffee.oauth.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcoffeeOauthAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcoffeeOauthAdminApplication.class, args);
+	}
+
+}
